2b7eac804e9f9d8dd230f8321fe2878665512d54
jeads <60721703+jeads92@users.noreply.github.com>
2022-07-05 12:01:35 -0400

    Removed debugging and testing functions.

--- ConwaysGameofLife/Form1.cs
+++ ConwaysGameofLife/Form1.cs
@@ -13,69 +13,6 @@ namespace ConwaysGameofLife
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            int xPosition = 0;
-            int yPosition = 0;
-            int xSize = 50;
-            int ySize = 50;
-            for(int i = 0; i < 10; i++)
-            {
-                Label labelGen = new Label();
-                labelGen.Text = " ";
-                labelGen.BackColor = Color.Chartreuse;
-                labelGen.Size = new Size(xSize, ySize);
-                labelGen.Location = new Point(xPosition, yPosition);
-                xPosition += xSize;
-                Controls.Add(labelGen);
-            }
-        }
-
-        private void testgrid_Click(object sender, EventArgs e)
-        {
-            Grid tester = new Grid(10, 10);
-            debugBox.Items.Add(tester.GetHeight());
-            debugBox.Items.Add(tester.GetWidth());
-        }
-
-        private void displayGrid(Grid gridObject)
-        {
-            panel1.Controls.Clear();
-            int xPosition = 0;
-            int yPosition = 0;
-            int xSize = 10;
-            int ySize = 10;
-            Random rand = new Random();
-            for (int i = 0; i < gridObject.GetHeight(); i++)
-            {
-                for (int j = 0; j < gridObject.GetWidth(); j++)
-                {
-                    Label lbl = new Label();
-                    lbl.Text = " ";
-                    if (rand.Next(0, 2) == 0)
-                    { 
-                        lbl.BackColor = Color.Chartreuse;
-                    }
-                    else
-                    {
-                        lbl.BackColor = Color.Black;
-                    }
-                    lbl.Size = new Size(xSize, ySize);
-                    lbl.Location = new Point(xPosition, yPosition);
-                    panel2.Controls.Add(lbl);
-                    xPosition += xSize;
-                }
-                xPosition = 0;
-                yPosition += ySize;
-            }
-        }
-
-        private void createDisplay_Click(object sender, EventArgs e)
-        {
-            Grid tester = new Grid(5, 10);
-            displayGrid(tester);
-        }
-
         private void timer1_Tick(object sender, EventArgs e)
         {
             int squareWidth = 10;
@@ -116,54 +53,6 @@ namespace ConwaysGameofLife
 
             cellMatrix.Update();
 
-        }
-
-        private void timerbtn_Click(object sender, EventArgs e)
-        {
-            Grid tester = new Grid(10, 10);
-            displayGrid(tester);
-        }
-
-        private void clearpanel_Click(object sender, EventArgs e)
-        {
-            panel2.Controls.Clear();
-        }
-
-        private void changeColors_Click(object sender, EventArgs e)
-        {
-            foreach(Control control in panel2.Controls)
-            {
-                Label iterLabel = control as Label;
-                iterLabel.BackColor = Color.Purple;
-            }
-        }
-
-        private void paintButton_Click(object sender, EventArgs e)
-        {
-            int squareWidth = 10;
-            int squareHeight = 10;
-            int squareXLocation = 0;
-            int squareYLocation = 0;
-
-            SolidBrush myBrush = new SolidBrush(Color.RebeccaPurple);
-            Graphics formGraphics;
-            formGraphics = panel2.CreateGraphics();
-            formGraphics.FillRectangle(myBrush, new Rectangle(0, 0, 10, 10));
-
-            for (int i = 0; i < 25; i++)
-            {
-                for(int j = 0; j < 25; j++)
-                {
-                    formGraphics.FillRectangle(myBrush, new Rectangle(squareXLocation, squareYLocation, 10, 10));
-                    squareXLocation += squareWidth;
-                }
-                squareYLocation += squareHeight;
-                squareXLocation = 0;
-            }
-
-            // Dispose of the unwanted objects
-            myBrush.Dispose();
-            formGraphics.Dispose();
         }
     }
 }
